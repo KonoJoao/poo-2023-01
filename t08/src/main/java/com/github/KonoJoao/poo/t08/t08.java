3590c820b3a3f294b8dbaccfe799c4f600da2340
Aluno <joaovitoralves2009@hotmail.com>
2023-05-31 20:08:33 -0300

    t08 inicio

---
+++ t08/src/main/java/com/github/KonoJoao/poo/t08/t08.java
@@ -0,0 +1,23 @@
+package com.github.KonoJoao.poo.t08;
+
+class Disciplina{
+    String nome;
+}
+
+class LinguagemProgramacao{
+
+}
+
+class Projeto{
+    LinguagemProgramacao[] linguagemUsada;
+}
+
+class Endereco{
+    String logradouro;
+    String cep;
+    String bairro;
+}
+
+class Escola{
+    Endereco endereco;
+}
