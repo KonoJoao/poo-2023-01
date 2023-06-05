4d780c02ec1f6b4c486248faa187a44b7e83a48d
JoaoUFG <joao.tavares@discente.ufg.br>
2023-06-05 20:33:58 -0300

    t08

---
+++ t08/src/main/java/com/github/KonoJoao/poo/t08/escola.java
@@ -0,0 +1,12 @@
+package com.github.konojoao.poo.t08;
+
+class Endereco{
+    String logradouro;
+    String cep;
+    String bairro;
+}
+
+class Escola{
+    Endereco endereco;
+    int capacidadeAlunos;
+}
