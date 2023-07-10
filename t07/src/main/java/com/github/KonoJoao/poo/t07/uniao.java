d2b4b344a7b8be37a08cd1ba3fec178a75339f8d
João Vitor Alves dos Reis <joaovitoralves2009@hotmail.com>
2023-07-10 15:30:45 -0300

    diretorio base corrigido e t07

--- t07/src/main/java/com/github/KonoJoao/poo/t07/uniao.java
+++ t07/src/main/java/com/github/KonoJoao/poo/t07/uniao.java
@@ -2,14 +2,16 @@ package com.github.konojoao.poo.t07;
 
 public class Uniao {
     private ArrayList filhos;
-    private Pessoa parceiros[2];
+    private Pessoa[] parceiros;
 
     public void novoFilho(Pessoa P){
         filhos.add(p);
     }
 
     public Uniao(Pessoa a, Pessoa b){
+        parceiros[0] = new Pessoa();
         parceiros[0] = a;
+        parceiros[1] = new Pessoa();
         parceiros[1] = b;
     }
 }
