a94c311309de895d220bb8b7a1d1bda33c50c08b
João Vitor Alves dos Reis <joaovitoralves2009@hotmail.com>
2023-07-24 13:14:17 -0300

    t07 p3

---
+++ t17/src/main/java/com/github/konojoao/poo/t17/Pessoa.java
@@ -0,0 +1,8 @@
+package com.github.konojoao.poo.t17;
+
+public class Pessoa {
+    private Danca danca;
+    private Convite convite;
+    private Boolean genero;
+    private Boolean acompanhante;
+}
---
+++ t17/src/main/java/com/github/konojoao/poo/t17/Tarefa.java
@@ -0,0 +1,5 @@
+package com.github.konojoao.poo.t17;
+
+public class Tarefa {
+    private Trabalhador trabalhadorResponsavel;
+}
