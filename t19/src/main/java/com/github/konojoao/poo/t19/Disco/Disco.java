af2ffd6281859ce620a8a1f3f92e21b7c56282c4
João Vitor Alves dos Reis <joaovitoralves2009@hotmail.com>
2023-08-01 14:58:13 -0300

    t19 p5

---
+++ t19/src/main/java/com/github/konojoao/poo/t19/Disco/Disco.java
@@ -0,0 +1,5 @@
+package com.github.konojoao.poo.t19.disco;
+
+public class Disco {
+    private Arquivo arquivo;
+}
---
+++ t19/src/main/java/com/github/konojoao/poo/t19/Disco/Permissao.java
@@ -0,0 +1,7 @@
+package com.github.konojoao.poo.t19.disco;
+
+public class Permissao {
+    private Boolean leitura;
+    private Boolean escrita;
+    private Boolean execucao;
+}
